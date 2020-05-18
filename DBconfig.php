c38acd0c4126c32e9db6a494a9ba56311ca70be4
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2020-05-18 09:49:55 +0200

    added db connection and created index file

---
+++ DBconfig.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+DEFINE("DB_USER", "root");
+DEFINE("DB_PASS", "root");
+
+//Met een try catch block worden zo geheten exceptions of uitzonderingen opgevangen en wordt gezocht naar een catch met een code die de exception kan uitvoeren, als deze niet gevonden kan worden wordt hiervoor de global exception handler voor gebruikt.
+
+try {
+    $db = new PDO("mysql:host=localhost;dbname=examen", DB_USER, DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
