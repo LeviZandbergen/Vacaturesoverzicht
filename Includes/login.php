b30b9c796e6ffee073714596fbc8ee02d259bf57
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2020-05-18 12:40:41 +0200

    added register functionality

--- Includes/login.php
+++ Includes/login.php
@@ -6,14 +6,14 @@
             <h3>Inloggen</h3>
             <a>Email</a><input type="email" class="input-text" name="email"><br><br>
             <a>Wachtwoord</a><input type="password" class="input-text" name="password"<br><br>
-            <button type="submit" class="button login-register-button" name="submit">Login</button>
+            <button type="submit" class="button login-register-button" name="login">Inloggen</button>
         </form>
 
         <form name="inloggen" id="register" method="POST" enctype="multipart/form-data" action="">
             <h3>Registreer een nieuw account</h3>
             <a>Email: </a><input type="email" class="input-text" name="email"><br><br>
             <a>Wachtwoord: </a><input type="password" class="input-text" name="password"><br><br>
-            <button type="submit" class="button login-register-button" name="submit">Login</button>
+            <button type="submit" class="button login-register-button" name="register">Registreren</button>
         </form>
     </div>
 </div>
--- Login.php
+++ Login.php
@@ -4,13 +4,14 @@
 include('Includes\header.php');
 include('Includes\login.php');
 
-if (isset($_POST["submit"])) {
+if (isset($_POST["login"])) {
     login();
+} elseif (isset($_POST["register"])) {
+    register();
 }
 
 function login()
 {
-    include('DBconfig.php');
     include('DBconfig.php');
     $error = "";
     $email = htmlspecialchars($_POST["email"]);
@@ -37,6 +38,34 @@ function login()
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
+    echo '<script language="javascript">';
+    echo 'alert("' . $error . '")';
+    echo '</script>';
+}
+
+function register()
+{
+    include('DBconfig.php');
+    $error = "";
+
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+    $validatedEmail = filter_var($email, FILTER_VALIDATE_EMAIL);
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $sql = "SELECT * FROM user WHERE email = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array($email));
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($result > 0) {
+        $error .= "Dit emailadres is al in gebruik";
+    } else {
+        $query = "INSERT INTO user (email, password) VALUES ('$validatedEmail', '$hashedPassword')";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $error .= "gebruiker succesvol aangemaakt";
+    }
+
     echo '<script language="javascript">';
     echo 'alert("' . $error . '")';
     echo '</script>';
